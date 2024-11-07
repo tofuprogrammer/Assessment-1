aed791324dde1286081669fa343b8833725b7168
h011771o <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-07 10:08:52 +0000

    Started Dynamic inventory task

--- Dynamic inventory/Dynamic inventory.cpp
+++ Dynamic inventory/Dynamic inventory.cpp
@@ -1,6 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, char* argv[])
+int main()
 {
-    
-    return 0;
+    int inventorySize;
+        std::cout << "Enter the size of your inventory (1-16): ";
+        std::cin >> inventorySize;
+    while (std::cin.fail() || inventorySize < 1 || inventorySize > 16) // Flushes the cin buffer and prompts the user for another number if their input was invalid
+    // The user's input is invalid if it is not a number, a number less than 1, or a number greater than 16
+    {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cout << "Error: Invalid input. Enter a number between 1 and 16: ";
+        std::cin >> inventorySize;
+    }
+    std::cout << inventorySize;
+    std::vector<std::string> arr(inventorySize);
+    std::cout << "Initialised inventory with " << inventorySize << " slots." << '\n';
+    std::cout << "Type \"help\" for a list of all commands" << '\n';
 }
