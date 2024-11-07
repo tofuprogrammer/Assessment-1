21f36070dc0f61208038f4999e6f1861353c3eed
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-07 18:50:52 +0000

    Scrapped dynamic inventory work

--- Dynamic inventory/Dynamic inventory.cpp
+++ Dynamic inventory/Dynamic inventory.cpp
@@ -1,34 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-#include "Functions (Dynamic inventory).hpp"
-
-class PlayerInventory
-{
-    
-};
-
 int main()
 {
-    int inventorySize;
-        std::cout << "Enter the size of your inventory (1-16): ";
-        std::cin >> inventorySize;
-    while (std::cin.fail() || inventorySize < 1 || inventorySize > 16) // Flushes the cin buffer and prompts the user for another number if their input was invalid
-    // The user's input is invalid if it is not a number, a number less than 1, or a number greater than 16
-    {
-        std::cin.clear();
-        std::cin.ignore(1000, '\n');
-        std::cout << "Error: Invalid input. Enter a number between 1 and 16: ";
-        std::cin >> inventorySize;
-    }
-    std::vector<std::string> playerInventory(inventorySize);
-    std::cout << "Initialised inventory with " << inventorySize << " slots." << '\n';
-    std::cout << "Type \"help\" for a list of all commands" << '\n';
-    std::string userInput;
-    std::cin >> userInput;
-    if (userInput == "help")
-        helpUser();
-    if (userInput == "exit")
-        exitProgram();
+
 }
