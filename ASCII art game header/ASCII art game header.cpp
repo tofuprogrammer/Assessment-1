c37d9f7594f950344364dab01a36140715934c74
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-07 20:01:28 +0000

    Fixed punctuation in comments for ASCII art game header.cpp.

--- ASCII art game header/ASCII art game header.cpp
+++ ASCII art game header/ASCII art game header.cpp
@@ -1,8 +1,8 @@
-#include <iostream> // Allows for input / output operations such as cin and cout
+#include <iostream> // Allows for input / output operations such as cin and cout.
 
-int main() // Code begins executing from here
+int main() // Code begins executing from here.
 {
-    // Outputs boat image
+    // Outputs boat image.
     std::cout << "                 |" << '\n';
     std::cout << "                /|\\" << '\n';
     std::cout << "               / | \\    a boat" << '\n';
