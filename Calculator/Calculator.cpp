e46f1ec5e7de44faf22e40fbb5f5ee95d2bacbae
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-07 20:09:58 +0000

    Fixed some punctuation in comments and outputs

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Enter a number: ";
     std::cin >> number; // Assigns the user's input to the double declared on line 7 (number).
 
-    if (std::cin.fail()) // Clears user's input and prompts user for input again if invalid (not a number)
+    if (std::cin.fail()) // Clears user's input and prompts user for input again if invalid (not a number).
     {
         std::cerr << "Invalid input. You must enter a number. The program will now exit...";
         std::cin.clear();
@@ -16,7 +16,7 @@ int main()
         return 1;
     }
 
-    // Creates variables that store the result of each operation on the number input by the user
+    // Creates variables that store the result of each operation on the number input by the user.
     double negation = number * (-1);
     double absolute_value = abs(number);
     double square = pow(number, 2);
@@ -26,13 +26,13 @@ int main()
     double ceiling = std::ceil(number);
     double rounded = std::round(number);
 
-    std::cout << "The number you specified is " << number << '\n'; // Outputs the number input by the user
-    if (number < 0) // Checks if the number the user input is negative
+    std::cout << "The number you specified is " << number << '\n'; // Outputs the number input by the user.
+    if (number < 0) // Checks if the number the user input is negative.
     {
-        std::cout << "This is a negative number, so the absolute value of " << number << " (" << absolute_value << ") will be used to calculate the square root" << '\n';
+        std::cout << "This is a negative number, so the absolute value of " << number << " (" << absolute_value << ") will be used to calculate the square root." << '\n';
     }
 
-    // Outputs results
+    // Outputs results.
     std::cout << "The negation value of " << number << " is " << negation << '\n';
     std::cout << "The absolute value of " << number << " is " << absolute_value << '\n';
     std::cout << "The square of " << number << " is " << square << '\n';
